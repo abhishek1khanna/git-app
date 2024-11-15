823e577b47bbd6f9ca1be37d61f4bc6c1841ea4e
abhishek1khanna <140689240+abhishek1khanna@users.noreply.github.com>
2024-11-15 22:06:46 +0530

    changes

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+try{
+    const port = process.env.PORT;
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+}catch(e){
+    console.error(e);
+    process.exit(1);
+}
