ef90747f67d66e161cd0a3464840f5269a8fe9fa
abhishek1khanna <140689240+abhishek1khanna@users.noreply.github.com>
2024-11-15 22:39:23 +0530

    changes

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 try{
+
+    app.get('/receive', async (req, res) => {
+          return res.status(200).send('RabbitMQ channel is not initialized');
+    });
+
     console.log('server starting ... ');
     const port = process.env.PORT;
     app.listen(port, () => console.log(`Server running on port ${port}`));
