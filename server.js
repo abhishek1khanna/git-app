d932c570271ef14351e629210192798e4f519599
abhishek1khanna <140689240+abhishek1khanna@users.noreply.github.com>
2024-11-16 13:51:24 +0530

    changes

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ app.use(express.json());
 try{
 
     app.get('/receive', async (req, res) => {
-          return res.status(200).send('RabbitMQ channel is not initialized !!!! OKK');
+          return res.status(200).send('RabbitMQ channel is not initialized !!!! OKK updated very nice updated once again');
     });
+
+
+    app.get('/db', async (req, res) => {
+        try {
+          const conn = await mongoose.connect(process.env.MONGODB_URI);
+          if (conn) {
+            res.status(200).send(`Connected to DB. Environment: ${process.env.NODE_ENV}, Port: ${process.env.PORT}, DB: ${process.env.MONGODB_URI} `);
+          }
+        } catch (err) {
+          res.send(`Environment: ${process.env.NODE_ENV}, Port: ${process.env.PORT}, DB: ${process.env.MONGODB_URI}, Error: ${err}`);
+        }
+    })   
 
     console.log('server starting ... ');
     const port = process.env.PORT;
